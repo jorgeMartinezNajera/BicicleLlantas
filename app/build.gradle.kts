9723e16dddcd03a21310660dccafa355801bdfd6
max00p2 <max_car12344321@hotmail.com>
2025-05-17 19:25:06 -0600

    prueba añadi 6 lineas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,3 +50,14 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+
+
+
+
+
+
+
+
+
+
