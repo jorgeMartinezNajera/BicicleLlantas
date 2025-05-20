a4bf1dbc13e3584f8639f47db1a6de3206a45f63
brisahernandezreyes12345 <116373778+brisahernandezreyes12345@users.noreply.github.com>
2025-05-20 02:30:37 -0600

    Cambio en Login y fin de compras
    En en el archivo de de login .xml le agregue mas espacio entre cajas y logo. En fin de compras cambie el logo y le agregue el boton que nos regresa a el catalogo

--- app/src/main/java/com/example/appventasequipo4/activity_details.kt
+++ app/src/main/java/com/example/appventasequipo4/activity_details.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class activity_details : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/appventasequipo4/finalCompra.java
+++ app/src/main/java/com/example/appventasequipo4/finalCompra.java
@@ -2,8 +2,9 @@ package com.example.appventasequipo4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageButton;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -48,7 +49,19 @@ TextView resultado, resultado2,resultado3;
         Double tot = extras.getDouble("total");
         resultado3.setText("$"+tot.toString());
 
+        // Botón de retroceso
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(finalCompra.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+                startActivity(intent);
+                finish();
+            }
+        });
 
-   }
+
+    }
 }
 
